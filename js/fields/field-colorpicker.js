2720e38ffb05d7b6f8ddbfc88894ccf38ffd146f
oleg <oleg_8@ukr.net>
2016-04-06 10:04:16 +0300

    colorpicker

--- js/fields/field-colorpicker.js
+++ js/fields/field-colorpicker.js
@@ -4,7 +4,8 @@ Fields.colorpicker = Backbone.View.extend(
     className: "settings-item",
     events: {
         'change input': 'changeInput',
-        'click .other-color': 'changeColor'
+        'click .other-color': 'changeColor',
+        'click .change-color': 'changeColorPicker'
     },
     /**
      * View field colorpicker
@@ -19,15 +20,16 @@ Fields.colorpicker = Backbone.View.extend(
      * @param {Object} evt
      */
     changeInput: function (evt) {
+        console.log('dd');
         var target = jQuery(evt.target);
-        console.log(this.model);
-        this.model.set(target.attr('name'), target.prev().find('.active').children().css('background-color'));
+        this.model.set(target.attr('name'), target.parent().find('.active').css('background-color'));
     },
     /**
      * Get value field colorpicker
      * @returns {String}
      */
     getValue: function () {
+        console.log('dd');
         return this.model.get(this.config.name) || this.config.default;
     },
     /**
@@ -35,36 +37,53 @@ Fields.colorpicker = Backbone.View.extend(
      * @returns {String}
      */
     getOherColors: function () {
+        console.log(this.config.colors[i]);
+        console.log(this.getValue());
         var colors = '';
         for (var i = 0; i < this.config.colors.length; i++) {
-            colors += '<div class="other-color ' + (this.config.colors[i] == this.getValue() ? 'active' : '') + '" style="background-color: ' + this.config.colors[i] +'"><span ></span></div>';
+            colors += '<div class="other-color theme-colors ' + (this.config.colors[i] == this.getValue() ? 'active' : '') + '" style="background: ' + this.config.colors[i] +'"><span ></span></div>';
         }
         return '<div class="other-colors">' + colors + '</div>';
+    },
+    /**
+     * Change color with colorpicker
+     * @param {Object} evt
+     */
+    changeColorPicker: function (evt) {
+        console.log('single-colors ' + this.config.colors);
+        console.log('values ' + this.getValue())
+        var elem = jQuery(evt.currentTarget);
+        this.$el.find('.other-color').removeClass('active');
+        elem.addClass('active');
+        this.$el.find('input').trigger("change");
+        
+        var name = elem.closest('.settings-item').find('input').prop('name');
+        var model = this.model;
+        elem.on('slidermove', function() {
+            elem.addClass('active');
+            model.set(name, elem.css('background-color'));
+        });
     },
     /**
      * Change other image
      * @param {Object} evt
      */
     changeColor: function (evt) {
+        console.log('dd');
         var elem = jQuery(evt.currentTarget);
         this.$el.find('.other-color').removeClass('active');
         elem.addClass('active');
         this.$el.find('input').trigger("change");
-//        this.$el.find('.edit-image img').attr('src', elem.find('img').attr('src'));
-//        console.log(this.$el.find('input').trigger("change"));
-//        this.$el.find('input').trigger("change");
-//        if (!this.$el.find('.edit-image').is(":visible")) {
-//            this.$el.find('.edit-image').show();
-//        }
     },
     /**
      * Create filed colorpicker
      * @returns {String}
      */
     create: function () {
+        console.log(this.config.colors );
         var block = '<div class="title">'+this.config.label+'</div>' + 
                  (this.config.colors ? this.getOherColors() : '') +
-                 '<button class="change-color other-color" data-wheelcolorpicker data-wcp-preview="true" data-wcp-sliders="wv" id="color-input"></button>' +
+                 '<button class="change-color other-color' + (this.config.colors[i] == this.getValue() ? 'active' : '') + '" data-wheelcolorpicker data-wcp-preview="true" data-wcp-sliders="wv" id="color-input"></button>' +
                  '<input type="hidden" name="' + this.config.name + '" value="' + this.getValue() + '">'
                 
         var colorpicker = "<script type='text/javascript'>" +
