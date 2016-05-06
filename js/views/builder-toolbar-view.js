e822135a9f1320ea4051da634026a0dcbe3ca658
Maksym Pomazan <maxazan@gmail.com>
2016-05-06 17:05:24 +0300

    toolvar test

--- js/views/builder-toolbar-view.js
+++ js/views/builder-toolbar-view.js
@@ -3,110 +3,104 @@
  *
  * @type @exp;Backbone@pro;View@call;extend
  */
- if (typeof module !== 'undefined' && module.exports) {
-     var Backbone = require('backbone');
-}
-var BuilderToolbarView = Backbone.View.extend(
+
+var BuilderToolbarView = Backbone.View.extend({
     /** @lends BuilderMenuGroupsView.prototype */
-    {
-        tagName: 'div',
-        attributes: function() {
-            return {
-                id: "builder-toolbar"
-            };
-        },
-        events: {
-            'click .preview-mode-button': 'clickPreviewMode',
-            'click .device-mode-button': 'clickDeviceMode',
-            'click .exit-button': 'clickExit',
-            'click .save-button': 'clickSave',
-            'click .autosave-checkbox': 'clickAutosave'
-        },
-        /**
-         * View toolbar
-         * @class BuilderToolbarView
-         * @augments Backbone.View
-         * @constructs
-         */
-        initialize: function(options) {
-            this.storage = options.storage;
-            this.controller = options.controller;
-        },
-        /**
-         * Render toolbar
-         * @returns {Object}
-         */
-        render: function() {
-            this.$el.html(_.template(this.storage.getBuilderTemplate('builder-toolbar'))());
-            return this;
-        },
-        setPreviewMode: function() {
-            this.$el.fadeOut(300);
-        },
-        setEditMode: function() {
-            this.$el.fadeIn(300);
-        },
-        setDeviceMode: function(mode) {
-            this.$el.find('.device-mode-button').removeClass('active');
-            this.$el.find('.device-mode-button[name=' + mode + ']').addClass('active');
-        },
+    tagName: 'div',
+    events: {
+        'click .preview-mode-button': 'clickPreviewMode',
+        'click .device-mode-button': 'clickDeviceMode',
+        'click .exit-button': 'clickExit',
+        'click .save-button': 'clickSave',
+        'click .autosave-checkbox': 'clickAutosave'
+    },
+    attributes: function() {
+        return {
+            id: "builder-toolbar"
+        };
+    },
+    /**
+     * View toolbar
+     * @class BuilderToolbarView
+     * @augments Backbone.View
+     * @constructs
+     */
+    initialize: function(options) {
+        this.storage = options.storage;
+        this.controller = options.controller;
+    },
+    /**
+     * Render toolbar
+     * @returns {Object}
+     */
+    render: function() {
+        this.$el.html(_.template(this.storage.getBuilderTemplate('builder-toolbar'))());
+        return this;
+    },
+    /**
+     * Resize toolbar
+     */
+    resize: function() {
+        this.$el.css({
+            width: window.innerWidth
+        });
+        return this;
+    },
+    /**
+     * Logo rotation
+     * @param {Integer} side
+     */
+    logoRotation: function(side) {
+        this.$el.find('.logo')
+            .removeClass(function(index, css) {
+                return (css.match(/\bside-\S+/g) || []).join(' ');
+            })
+            .addClass(side);
+    },
+    setPreviewMode: function() {
+        this.$el.fadeOut(300);
+    },
+    setEditMode: function() {
+        this.$el.fadeIn(300);
+    },
+    setDeviceMode: function(mode) {
+        this.$el.find('.device-mode-button').removeClass('active');
+        this.$el.find('.device-mode-button[name=' + mode + ']').addClass('active');
+    },
+    startEditBlock: function(blockId) {
+        this.logoRotation('side-270');
+    },
+    /**
+     * Show loader autosave
+     */
+    showSaveLoader: function() {
+        this.$el.find('.save-button span.text').hide();
+        this.$el.find('.save-button .clock').css('display', 'block');
+    },
+    /**
+     * Hide loader autosave
+     */
+    hideSaveLoader: function() {
+        this.$el.find('.save-button .clock').css('display', '');
+        this.$el.find('.save-button span.text').show();
+    },
 
-        /**
-         * Resize toolbar
-         */
-        resize: function() {
-            this.$el.css({
-                width: jQuery(window).width()
-            });
-        },
-        //EVENTS
-        clickPreviewMode: function() {
-            this.controller.setPreviewMode();
-        },
-        clickDeviceMode: function(evt){
-            this.controller.setDeviceMode(evt.target.name);
-        },
-        clickExit:function(){
-            this.controller.exit();
-        },
-        clickSave:function(){
-            this.controller.save();
-        },
-        clickAutosave: function(evt){
-            this.controller.setAutoSave(evt.target.checked);
-        },
+    //EVENTS
+    clickPreviewMode: function() {
+        this.controller.setPreviewMode();
+    },
+    clickDeviceMode: function(evt) {
+        this.controller.setDeviceMode(evt.target.name);
+    },
+    clickExit: function() {
+        this.controller.exit();
+    },
+    clickSave: function() {
+        this.controller.save();
+    },
+    clickAutosave: function(evt) {
+        this.controller.setAutoSave(evt.target.checked);
+    },
 
-        startEditBlock: function(blockId){
-            this.logoRotation('side-270');
-        },
-        /**
-         * Logo rotation
-         * @param {Integer} side
-         */
-        logoRotation: function(side) {
-            // rotate cube logo
-            this.$el.find('.logo')
-                .removeClass(function(index, css) {
-                    return (css.match(/\bside-\S+/g) || []).join(' ');
-                })
-                .addClass(side);
-        },
-        /**
-         * Show loader autosave
-         */
-        showSaveLoader: function () {
-            this.$el.find('.save-button span.text').hide();
-            this.$el.find('.save-button .clock').css('display', 'block');
-        },
-        /**
-         * Hide loader autosave
-         */
-        hideSaveLoader: function () {
-            this.$el.find('.save-button .clock').css('display', '');
-            this.$el.find('.save-button span.text').show();
-        }
-    });
 
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = BuilderToolbarView;
-}
+});
