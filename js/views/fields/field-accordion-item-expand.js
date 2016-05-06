bb330c8e69193f04036e4a0c444542b3d7365519
vladpolonskiy <opolo93@mail.ru>
2016-05-06 17:07:37 +0300

    accordion refactor started

--- js/views/fields/field-accordion-item-expand.js
+++ js/views/fields/field-accordion-item-expand.js
@@ -1,61 +1,63 @@
 var Fields = Fields || {};
 Fields.accordion_item_expand = Backbone.View.extend(
-/** @lends Fields.accordion_item.prototype */{
-    className: "settings-item settings-accordion",
-    accordionItemTpl : null,
-    events: {
-        'click .cross-delete': 'deleteModel'
-    },
-    /**
-     * View field accordion item
-     * needed for field accordion
-     * @class Fields.image
-     * @augments Backbone.View
-     * @constructs
-     */
-    initialize: function () {
-        this.$el.attr('data-model-id', this.model.id);
-        var self = this;
-        builder.storage.getBuilderTemplate('field-accordion-item-expand', function(err, data){
-            self.accordionItemTpl = _.template(data);
-        });
-    },
-    /**
-     * Render filed accordion_item
-     * @returns {Object}
-     */
-    render: function () {
-        var items = [];
-        var settingsView = new FieldsView({model: this.model});
-        settingsView.config = this.config;
+        /** @lends Fields.accordion_item.prototype */{
+            className: "settings-item settings-accordion",
+            accordionItemTpl: null,
+            events: {
+                'click .cross-delete': 'deleteModel'
+            },
+            /**
+             * View field accordion item
+             * needed for field accordion
+             * @class Fields.image
+             * @augments Backbone.View
+             * @constructs
+             */
+            initialize: function (options) {
+                this.model = options.model;
+                this.settings = options.settings;
+                this.storage = options.storage;
+                this.controller = options.controller;
+                this.$el.attr('data-model-id', this.model.id);
+                this.tpl = _.template(this.storage.builderTemplates['field-accordion-item-expand']);
+            },
+            /**
+             * Render filed accordion_item
+             * @returns {Object}
+             */
+            render: function () {
+                var items = [],
+                        settingsView = new FieldsView({
+                            model: this.model,
+                            settings: this.settigns
+                        }),
+                        htmldata = {
+                            "image": settingsView.model.get('image'),
+                            "title": settingsView.model.get('title')
+                        };
 
-        var htmldata = {
-            "image" : settingsView.model.get('image'),
-            "title" : settingsView.model.get('title')
-        };
-        
-        this.listenTo(this.model, 'change', function (){
-            this.$el.find("h3 span.text").first().html(this.model.get('title'));
-            this.$el.find("h3 span.preview_img img").first().prop('src', this.model.get('image'));
-        });
-        
-        items.push(this.accordionItemTpl( htmldata ));
-        items.push(settingsView.render().el);
+                this.listenTo(this.model, 'change', function () {
+                    this.$el.find("h3 span.text").first().html(this.model.get('title'));
+                    this.$el.find("h3 span.preview_img img").first().prop('src', this.model.get('image'));
+                });
 
-        if (typeof (this.config.show) == "undefined" || this.config.show(this.model)) {
-            this.$el.html(items);
-        }
-        return this;
-    },
-    /**
-     * Removes the view from the DOM and unbinds all events.
-     * @param {Object} e
-     */
-    deleteModel: function (e) {
-        e.preventDefault();
-        this.model.stopListening();
-        this.model.trigger('destroy', this.model, this.model.collection);
-        this.remove();
-        this.model.trigger('change');
-    }
-});
+                items.push(this.tpl(htmldata));
+                items.push(settingsView.render().el);
+
+                if (typeof (this.config.show) == "undefined" || this.config.show(this.model)) {
+                    this.$el.html(items);
+                }
+                return this;
+            },
+            /**
+             * Removes the view from the DOM and unbinds all events.
+             * @param {Object} e
+             */
+            deleteModel: function (e) {
+                e.preventDefault();
+                this.model.stopListening();
+                this.model.trigger('destroy', this.model, this.model.collection);
+                this.remove();
+                this.model.trigger('change');
+            }
+        });
--- js/views/fields/field-accordion.js
+++ js/views/fields/field-accordion.js
@@ -1,141 +1,137 @@
 var Fields = Fields || {};
 Fields.accordion = Backbone.View.extend(
-/** @lends Fields.accordion.prototype */{
-    className: "settings-item",
-    uniqueId: null,
-    accordionTpl : null,
-    classNameItem: "",
-    events: {
-        'click .add-block': 'addNewItem',
-        'drop': 'changePosition'
-    },
-    /**
-     * View field accordion
-     * @class Fields.accordion
-     * @augments Backbone.View
-     * @constructs
-     */
-    initialize: function () {
-        var self = this;
-        builder.storage.getBuilderTemplate('field-accordion', function(err, data){
-            self.accordionTpl = _.template(data);
+        /** @lends Fields.accordion.prototype */{
+            className: "settings-item",
+            uniqueId: null,
+            tpl: null,
+            classNameItem: "",
+            events: {
+                'click .add-block': 'addNewItem',
+                'drop': 'changePosition'
+            },
+            /**
+             * View field accordion
+             * @class Fields.accordion
+             * @augments Backbone.View
+             * @constructs
+             */
+            initialize: function (options) {
+                this.model = options.model;
+                this.settings = options.settings;
+                this.storage = options.storage;
+                this.controller = options.controller;
+                this.tpl = _.template(this.storage.builderTemplates['field-accordion']);
+            },
+            /**
+             * Change position blocks accordion
+             * @param {Object} event
+             * @param {integer} position
+             */
+            changePosition: function (event, position) {
+                var self = this,
+                        values = this.getValue(),
+                        blocks = this.$el.find('#' + this.getUniqueId()).children('.settings-accordion');
+
+                blocks.each(function (index, listItem) {
+                    var dataId = self.$(listItem).data('model-id'),
+                            model = values.get(dataId);
+                    model.set('order', self.$(listItem).index() - 1);
+                });
+            },
+            /**
+             * Get value field accordion
+             * @returns {String}
+             */
+            getValue: function () {
+                return this.model.get(this.settings.name) || this.settings.default;
+            },
+            /**
+             * Get unique id
+             * @returns {String}
+             */
+            getUniqueId: function () {
+                return this.uniqueId = this.uniqueId || _.uniqueId('accordion-');
+            },
+            /**
+             * Add new item to accordion
+             * @param {Object} e
+             */
+            addNewItem: function (e) {
+                e.preventDefault();
+
+                var self = this,
+                        values = this.getValue(),
+                        settings = this.settigns.settings,
+                        settingsParams = [],
+                        data = [];
+
+                for (var i in settings) {
+                    settingsParams.push({'name': settings[i].name, 'default': settings[i].default});
+                }
+
+                for (var i = 0; i < settingsParams.length; i++) {
+                    data[settingsParams[i].name] = settingsParams[i].default;
+                }
+
+                values.add(BuilderUtils.createModel(data));
+                var item = new Fields[this.classNameItem]({
+                    model: BuilderUtils.createModel(data),
+                    settings: this.settings,
+                    storage: this.storage,
+                    controller: this.controller
+                });
+
+                this.model.listenTo(item.model, "change", function () {
+                    this.trigger('change');
+                    self.changePosition();
+                });
+
+                item.model.set('order', (values.models ? values.models.length - 1 : 0));
+
+                this.$el.find("#" + this.getUniqueId()).append(item.render().el);
+                this.$el.find("#" + this.getUniqueId()).accordion("refresh");
+                values.trigger('change');
+            },
+            /**
+             * Render filed accordion
+             * @returns {Object}
+             */
+            render: function () {
+                var values = this.getValue(),
+                        settings = this.settings.settings,
+                        items = [],
+                        valueModels = values.models;
+                // sort accordion settings
+                valueModels = _.sortBy(valueModels, function (model) {
+                    return model.get('order');
+                });
+                this.classNameItem = (this.settings.viewType === undefined || this.settings.viewType === "expand") ? 'accordion_item_expand' : 'accordion_item_flip';
+
+                for (var i = 0; i < valueModels.length; i++) {
+                    var item = new Fields[this.classNameItem]({
+                        model: valueModels[i],
+                        settings: this.settings,
+                        storage: this.storage,
+                        controller: this.controller
+                    });
+                    
+                    this.model.listenTo(item.model, "change", function () {
+                        this.trigger('change');
+                        this.changePosition();
+                    });
+                    
+                    items.push(item.render().el);
+                }
+
+                var htmldata = {
+                    "label": this.settings.label,
+                    "uniqueId": this.getUniqueId(),
+                    "settings": settings
+                };
+
+                if (typeof (this.settings.show) == "undefined" || this.settings.show(this.model)) {
+                    this.$el.html(this.accordionTpl(htmldata)).find('#' + this.getUniqueId()).append(items);
+                }
+                return this;
+            }
         });
-    },
-    /**
-     * Change position blocks accordion
-     * @param {Object} event
-     * @param {integer} position
-     */
-    changePosition: function (event, position) {
-        var values = this.getValue();
-        var blocks = jQuery('#' + this.getUniqueId()).children( '.settings-accordion');
-
-        blocks.each(function (index, listItem) {
-            var dataId = jQuery(listItem).data('model-id');
-            var model = values.get(dataId);
-            model.set('order', jQuery(listItem).index()-1);
-        });
-    },
-    /**
-     * Get value field accordion
-     * @returns {String}
-     */
-    getValue: function () {
-        return this.model.get(this.config.name) || this.config.default;
-    },
-    /**
-     * Get unique id
-     * @returns {String}
-     */
-    getUniqueId: function () {
-        return this.uniqueId = this.uniqueId || _.uniqueId('accordion-');
-    },
-    
-    /**
-     * Add new item to accordion
-     * @param {Object} e
-     */
-    addNewItem: function (e) {
-        e.preventDefault();
-        var self = this;
-        var values = this.getValue();
-        var settings = this.config.settings;
-
-        var settingsParams = [];
-        for (var i in settings) {
-            settingsParams.push({'name': settings[i].name, 'default': settings[i].default});
-        }
-
-        var data = [];
-        for (var i = 0; i < settingsParams.length; i++) {
-            data[settingsParams[i].name] = settingsParams[i].default;
-        }
-
-        var model = BuilderUtils.createModel(data);
-
-        values.add(model);
-
-        var item = new Fields[this.classNameItem]({model: model});
-        
-        item.config = settings;
-
-        this.model.listenTo(item.model, "change", function () {
-            this.trigger('change');
-            self.changePosition();
-        });
-        
-        item.model.set('order', (values.models ? values.models.length-1 : 0));
-
-
-        jQuery("#" + this.getUniqueId()).append(item.render().el);
-        jQuery("#" + this.getUniqueId()).accordion("refresh");
-        values.trigger('change');
-    },
-    
-    /**
-     * Render filed accordion
-     * @returns {Object}
-     */
-    render: function () {        
-        var values = this.getValue(),
-            settings = this.config.settings,
-            items = [],
-            value_models = values.models;
-        // sort accordion settings
-        value_models = _.sortBy(value_models, function(model){
-            return model.get('order');
-        });
-        
-        if(this.config.viewType === undefined || this.config.viewType === "expand"){
-            this.classNameItem = 'accordion_item_expand';
-        }else{
-            this.classNameItem = 'accordion_item_flip';
-        }
-        
-        for (var i = 0; i < value_models.length; i++) {
-            var item = new Fields[this.classNameItem]({model: value_models[i], frontsettings: this.config.frontsettings});
-            item.config = settings;
-            
-            items.push(item.render().el);
-            
-//            this.model.listenTo(item.model, "change", function () {
-//                this.trigger('change');
-//            });
-
-//            
-//            values.listenTo(item.model, "change", function () {
-//                self.changePosition();
-//            });
-        }
-        var htmldata = {
-            "label" : this.config.label,
-            "uniqueId" : this.getUniqueId(),
-            "settings" : settings
-        }
-   
-        if (typeof (this.config.show) == "undefined" || this.config.show(this.model)) {
-            this.$el.html(this.accordionTpl( htmldata )).find('#'+ this.getUniqueId()).append(items);
-        }
-        return this;
-    }
-});
