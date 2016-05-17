76add39f28f5d8ecf55f1729fd194196ac3772ae
portapro <uvarenkov@gmail.com>
2016-05-17 15:37:37 +0300

    add move block

--- js/models/page-model.js
+++ js/models/page-model.js
@@ -21,5 +21,24 @@ var PageModel = Backbone.Model.extend({
             var model = BuilderUtils.createModel(blocks[i]);
             this.addBlock(model);
         }
+    },
+    moveUp: function (model) {
+        var index = this.get('blocks').indexOf(model);
+        
+        if (index > 0) {
+            this.swap(index, index - 1);
+            this.trigger('block_moveup', model.id);
+        }
+    },
+    moveDown: function (model) {
+        var index = this.get('blocks').indexOf(model);
+    
+        if (index < this.get('blocks').models.length) {
+            this.swap(index, index + 1);
+            this.trigger('block_movedown', model.id);
+        }
+    },
+    swap: function (indexA, indexB) {
+        this.get('blocks').models[indexA] = this.get('blocks').models.splice(indexB, 1, this.get('blocks').models[indexA])[0];
     }
 });
