6555f5abae330a0d662b84daf556cc9c9b064164
vladpolonskiy <opolo93@mail.ru>
2016-06-17 17:25:01 +0300

    - loader media fix  - start developing accordion problems (struggling)

--- js/views/fields/field-accordion.js
+++ js/views/fields/field-accordion.js
@@ -1,118 +1,129 @@
 var Fields = Fields || {};
 Fields.accordion = FieldView.extend(
-        /** @lends Fields.accordion.prototype */{
-            uniqueId: null,
-            classNameItem: "",
-            accordionMenuViews: [],
-            events: {
-                'drop .accordion': 'changePosition'
-            },
-            /**
-             * View field accordion
-             * @class Fields.accordion
-             * @augments Backbone.View
-             * @constructs
-             */
-            initialize: function (options) {
-                FieldView.prototype.initialize.call(this, options);
-                this.tpl = _.template(this.storage.qoobTemplates['field-accordion-preview']);
-            },
-            /**
-             * On accordion remove deleting binded events 
-             * and triggering basic remove funcction
-             * 
-             */
-            remove: function () {
-                this.$el.find('#' + this.uniqueId).next('.add-block').off("click", this.addNewItem);
-                Backbone.View.prototype.remove.apply(this, arguments);
-            },
-            removeItem: function () {
-                var values = this.getValue();
-                values.trigger('change');
-                this.changePosition();
-            },
-            /**
-             * Change position blocks accordion
-             * @param {Object} event
-             * @param {integer} position
-             */
-            changePosition: function (event, position) {
-                var self = this,
-                        values = this.getValue(),
-                        blocks = this.$el.find('#' + this.getUniqueId()).children('.settings-accordion');
+    /** @lends Fields.accordion.prototype */
+    {
+        uniqueId: null,
+        classNameItem: "",
+        accordionMenuViews: [],
+        events: {
+            'drop .accordion': 'changePosition'
+        },
+        /**
+         * View field accordion
+         * @class Fields.accordion
+         * @augments Backbone.View
+         * @constructs
+         */
+        initialize: function(options) {
+            FieldView.prototype.initialize.call(this, options);
+            this.tpl = _.template(this.storage.qoobTemplates['field-accordion-preview']);
+        },
+        /**
+         * On accordion remove deleting binded events 
+         * and triggering basic remove funcction
+         * 
+         */
+        remove: function() {
+            this.$el.find('#' + this.uniqueId).next('.add-block').off("click", this.addNewItem);
+            Backbone.View.prototype.remove.apply(this, arguments);
+        },
+        removeItem: function() {
+            var values = this.getValue();
+            if (values.length === 0) {
+                console.log(this.model.get(this.settings.name));
+                // this.model.set(this.settings.name, '');
+            }
+            values.trigger('change');
+            this.changePosition();
+        },
+        /**
+         * Change position blocks accordion
+         * @param {Object} event
+         * @param {integer} position
+         */
+        changePosition: function(event, position) {
+            var self = this,
+                values = this.getValue(),
+                blocks = this.$el.find('#' + this.getUniqueId()).children('.settings-accordion');
 
-                blocks.each(function (index, listItem) {
-                    var dataId = self.$(listItem).data('model-id'),
-                            model = values.get(dataId);
+            blocks.each(function(index, listItem) {
+                var dataId = self.$(listItem).data('model-id'),
+                    model = values.get(dataId);
 
-                    if (model) {
-                        model.set('order', self.$(listItem).index() - 1);
-                    }
-                });
-            },
-            /**
-             * Get unique id
-             * @returns {String}
-             */
-            getUniqueId: function () {
-                return this.uniqueId = this.uniqueId || _.uniqueId('accordion-');
-            },
-            /**
-             * Add new item to accordion
-             * @param {Object} e
-             */
-            addNewItem: function (e) {
-                e.preventDefault();
-                var values = this.getValue(),
-                        settings = this.settings.settings,
-                        defaults = this.defaults[0],
-                        settingsParams = [],
-                        data = [],
-                        newModel;
-
-                for (var i in settings) {
-                    settingsParams.push({'name': settings[i].name, 'default': defaults[settings[i].name]});
+                if (model) {
+                    model.set('order', self.$(listItem).index() - 1);
                 }
+            });
+        },
+        /**
+         * Get unique id
+         * @returns {String}
+         */
+        getUniqueId: function() {
+            return this.uniqueId = this.uniqueId || _.uniqueId('accordion-');
+        },
+        /**
+         * Add new item to accordion
+         * @param {Object} e
+         */
+        addNewItem: function(e) {
+            e.preventDefault();
+            var values = this.getValue(),
+                settings = this.settings.settings,
+                defaults = this.defaults[0],
+                settingsParams = [],
+                data = [],
+                newModel;
 
-                for (var i = 0; i < settingsParams.length; i++) {
-                    data[settingsParams[i].name] = settingsParams[i].default;
-                }
-                newModel = QoobUtils.createModel(data);
-                newModel.owner_id = this.model.id;
-                                
-                values.add(newModel);
-                newModel.on("remove_item", this.removeItem.bind(this));
-                var item = new Fields[this.classNameItem]({
-                    model: newModel,
-                    settings: settings,
-                    storage: this.storage,
-                    defaults: defaults,
-                    controller: this.controller,
-                    parentId: this.model.id
+            for (var i in settings) {
+                settingsParams.push({ 'name': settings[i].name, 'default': defaults[settings[i].name] });
+            }
+
+            for (var i = 0; i < settingsParams.length; i++) {
+                data[settingsParams[i].name] = settingsParams[i].default;
+            }
+            newModel = QoobUtils.createModel(data);
+            newModel.owner_id = this.model.id;
+            
+            values.add(newModel);
+            if(!(values.length > 0)) {
+                values.listenTo(newModel, 'change', function() {
+                    this.trigger('change', this);
                 });
-                item.model.set('order', (values.models ? values.models.length - 1 : 0));
+            }
+            newModel.on("remove_item", this.removeItem.bind(this));
+            var item = new Fields[this.classNameItem]({
+                model: newModel,
+                settings: settings,
+                storage: this.storage,
+                defaults: defaults,
+                controller: this.controller,
+                parentId: this.model.id
+            });
+            item.model.set('order', (values.models ? values.models.length - 1 : 0));
 
-                this.accordionMenuViews.push(item);
+            this.accordionMenuViews.push(item);
 
-                this.$el.find("#" + this.getUniqueId()).append(item.render().el);
-                this.$el.find("#" + this.getUniqueId()).accordion("refresh");
-                values.trigger('change');
-            },
-            /**
-             * Render filed accordion
-             * @returns {Object}
-             */
-            render: function () {
-                var values = this.getValue(),
-                        settings = this.settings.settings,
-                        items = [];
+            this.$el.find("#" + this.getUniqueId()).append(item.render().el);
+            this.$el.find("#" + this.getUniqueId()).accordion("refresh");
+            values.trigger('change');
+        },
+        /**
+         * Render filed accordion
+         * @returns {Object}
+         */
+        render: function() {
+            var values = this.getValue(),
+                settings = this.settings.settings,
+                items = [];
 
-                // sort accordion settings
-                values.models = _.sortBy(values.models, function (model) {
-                    return model.get('order');
-                });
-                
-                this.classNameItem = (this.settings.viewType === undefined || this.settings.viewType === "expand") ? 'accordion_item_expand' : 'accordion_item_flip';
+            // sort accordion settings
+            values.models = _.sortBy(values.models, function(model) {
+                return model.get('order');
+            });
+
+            this.classNameItem = (this.settings.viewType === undefined || this.settings.viewType === "expand") ? 'accordion_item_expand' : 'accordion_item_flip';
+            if (values.length && values.length > 0) {
                 for (var i = 0; i < values.models.length; i++) {
                     var item = new Fields[this.classNameItem]({
                         model: values.models[i],
@@ -125,96 +136,95 @@ Fields.accordion = FieldView.extend(
                     this.accordionMenuViews.push(item);
 
                     items.push(item.render().el);
-                    
+
                     // listen trigger when remove item
                     values.models[i].on("remove_item", this.removeItem.bind(this));
                 }
-
-                var htmldata = {
-                    "label": this.settings.label,
-                    "uniqueId": this.getUniqueId(),
-                    "settings": settings
-                };
-
-                if (typeof (this.settings.show) == "undefined" || this.settings.show(this.model)) {
-                    this.$el.html(this.tpl(htmldata)).find('#' + this.getUniqueId()).append(items);
-                    this.sortableInit();
-                }
-                // AddNewItem func on clicking button
-                // we added this handler dynamiclly to prevent bubbling it to inner accordions
-                this.$el.find('#' + this.uniqueId).next('.add-block').on('click', this.addNewItem.bind(this));
-
-                return this;
-            },
-            sortableInit: function () {
-                var self = this,
-                        id = this.getUniqueId();
-
-                this.$el.find("#" + id).accordion({
-                    header: "> div > h3.inner-settings-expand",
-                    animate: 500,
-                    collapsible: true,
-                    active: false,
-                    heightStyle: 'content'
-                }).sortable({
-                    items: ".settings-accordion",
-                    revert: false,
-                    axis: "y",
-                    helper : 'clone',
-                    handle: ".drag-elem",
-                    connectWith: "#drop-" + id,
-                    //scroll: true,
-                    start: function (event, ui) {
-                        self.controller.layout.viewPort.getIframeContents().find(".droppable").css("visibility", "hidden");
-                        if (self.$(this).find(".tinyMCE").length) {
-                            self.$(this).find(".tinyMCE").each(function () {
-                                try {
-                                    tinymce.execCommand("mceRemoveEditor", false, self.$(this).attr("id"));
-                                } catch (e) {
-                                }
-                            });
-                        }
-                        jQuery(this).addClass('is-droppable');
-                    },
-                    stop: function (event, ui) {
-                        jQuery(this).removeClass('is-droppable');
-                        ui.item.trigger("drop", ui.item.index());
-                        // IE doesn't register the blur when sorting
-                        // so trigger focusout handlers to remove .ui-state-focus
-                        ui.item.children("h3").triggerHandler("focusout");
-                        // Refresh accordion to handle new order
-                        self.$(this).accordion("refresh");
-                        self.controller.layout.viewPort.getIframeContents().find(".droppable").removeAttr("style");
-                        // Refresh tinyMCE
-                        if (self.$(this).find(".tinyMCE").length) {
-                            self.$(this).find(".tinyMCE").each(function () {
-                                try {
-                                    tinymce.execCommand("mceAddEditor", true, self.$(this).attr("id"));
-                                } catch (e) {
-                                }
-                            });
-                        }
-                    }
-                });
-
-                this.$el.find("#drop-" + id).droppable({
-                    hoverClass: "ui-state-hover",
-                    drop: function (event, ui) {
-                        var modelId = ui.draggable.data('model-id');
-                        var model = self.getAccordionMenuViews(modelId);
-                        model.deleteModel();
-                    }
-                });
-            },
-            /**
-             * Get sccordionMenuViews by id
-             * @param {Number} id modelId
-             */
-            getAccordionMenuViews: function(id) {
-                for (var i = 0; i < this.accordionMenuViews.length; i++) {
-                    if (this.accordionMenuViews[i].model && this.accordionMenuViews[i].model.id == id) {
-                        return this.accordionMenuViews[i];
-                    }
-                };
             }
-        });
+
+            var htmldata = {
+                "label": this.settings.label,
+                "uniqueId": this.getUniqueId(),
+                "settings": settings
+            };
+
+            if (typeof(this.settings.show) == "undefined" || this.settings.show(this.model)) {
+                this.$el.html(this.tpl(htmldata)).find('#' + this.getUniqueId()).append(items);
+                this.sortableInit();
+            }
+            // AddNewItem func on clicking button
+            // we added this handler dynamiclly to prevent bubbling it to inner accordions
+            this.$el.find('#' + this.uniqueId).next('.add-block').on('click', this.addNewItem.bind(this));
+
+            return this;
+        },
+        sortableInit: function() {
+            var self = this,
+                id = this.getUniqueId();
+
+            this.$el.find("#" + id).accordion({
+                header: "> div > h3.inner-settings-expand",
+                animate: 500,
+                collapsible: true,
+                active: false,
+                heightStyle: 'content'
+            }).sortable({
+                items: ".settings-accordion",
+                revert: false,
+                axis: "y",
+                helper: 'clone',
+                handle: ".drag-elem",
+                connectWith: "#drop-" + id,
+                //scroll: true,
+                start: function(event, ui) {
+                    self.controller.layout.viewPort.getIframeContents().find(".droppable").css("visibility", "hidden");
+                    if (self.$(this).find(".tinyMCE").length) {
+                        self.$(this).find(".tinyMCE").each(function() {
+                            try {
+                                tinymce.execCommand("mceRemoveEditor", false, self.$(this).attr("id"));
+                            } catch (e) {}
+                        });
+                    }
+                    jQuery(this).addClass('is-droppable');
+                },
+                stop: function(event, ui) {
+                    jQuery(this).removeClass('is-droppable');
+                    ui.item.trigger("drop", ui.item.index());
+                    // IE doesn't register the blur when sorting
+                    // so trigger focusout handlers to remove .ui-state-focus
+                    ui.item.children("h3").triggerHandler("focusout");
+                    // Refresh accordion to handle new order
+                    self.$(this).accordion("refresh");
+                    self.controller.layout.viewPort.getIframeContents().find(".droppable").removeAttr("style");
+                    // Refresh tinyMCE
+                    if (self.$(this).find(".tinyMCE").length) {
+                        self.$(this).find(".tinyMCE").each(function() {
+                            try {
+                                tinymce.execCommand("mceAddEditor", true, self.$(this).attr("id"));
+                            } catch (e) {}
+                        });
+                    }
+                }
+            });
+
+            this.$el.find("#drop-" + id).droppable({
+                hoverClass: "ui-state-hover",
+                drop: function(event, ui) {
+                    var modelId = ui.draggable.data('model-id');
+                    var model = self.getAccordionMenuViews(modelId);
+                    model.deleteModel();
+                }
+            });
+        },
+        /**
+         * Get sccordionMenuViews by id
+         * @param {Number} id modelId
+         */
+        getAccordionMenuViews: function(id) {
+            for (var i = 0; i < this.accordionMenuViews.length; i++) {
+                if (this.accordionMenuViews[i].model && this.accordionMenuViews[i].model.id == id) {
+                    return this.accordionMenuViews[i];
+                }
+            };
+        }
+    });
