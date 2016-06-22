9c6a8e6469439408a4f092e4b98b394813353476
vladpolonskiy <opolo93@mail.ru>
2016-06-22 11:41:07 +0300

    - JSONParam method (qoob-utils.js) - fix in accordion - XHR on saving page data with sending empty arrays like '[]'

--- js/qoob-utils.js
+++ js/qoob-utils.js
@@ -80,5 +80,54 @@ var QoobUtils = {
 
         return values;
     },
+    /**
+     * Encode JSON request, including empty arrays
+     */
+    JSONParam: function(a) {
+        var s = [],
+            rbracket = /\[\]$/,
+            isArray = function(obj) {
+                return Object.prototype.toString.call(obj) === '[object Array]';
+            },
+            add = function(k, v) {
+                v = typeof v === 'function' ? v() : v === null ? '' : v === undefined ? '' : v;
+                s[s.length] = encodeURIComponent(k) + '=' + encodeURIComponent(v);
+            },
+            buildParams = function(prefix, obj) {
+                var i, len, key;
 
+                if (prefix) {
+                    if (isArray(obj)) {
+                        if (obj.length === 0) {
+                            s[s.length] = encodeURIComponent(prefix) + '=[]';
+                        } else {
+                            for (i = 0, len = obj.length; i < len; i++) {
+                                if (rbracket.test(prefix)) {
+                                    add(prefix, obj[i]);
+                                } else {
+                                    buildParams(prefix + '[' + (typeof obj[i] === 'object' ? i : '') + ']', obj[i]);
+                                }
+                            }
+                        }
+                    } else if (obj && String(obj) === '[object Object]') {
+                        for (key in obj) {
+                            buildParams(prefix + '[' + key + ']', obj[key]);
+                        }
+                    } else {
+                        add(prefix, obj);
+                    }
+                } else if (isArray(obj)) {
+                    for (i = 0, len = obj.length; i < len; i++) {
+                        add(obj[i].name, obj[i].value);
+                    }
+                } else {
+                    for (key in obj) {
+                        buildParams(key, obj[key]);
+                    }
+                }
+                return s;
+            };
+
+        return buildParams('', a).join('&').replace(/%20/g, '+');
+    }
 };
