8546e3d03a7900979d699a14dddd73bec8f8abe8
portapro <uvarenkov@gmail.com>
2017-02-20 16:20:59 +0200

    Взаимодействие с пользователем. Не введенный URL #1469

--- skins/black/js/views/qoob-manage-libs-view.js
+++ skins/black/js/views/qoob-manage-libs-view.js
@@ -7,16 +7,18 @@ var QoobManageLibsView = Backbone.View.extend( // eslint-disable-line no-unused-
     /** @lends QoobManageLibsView.prototype */
     {
         events: {
+            'keyup .input-url': 'keyupInput',
+            'keydown': 'keyAction'
             'click .add-library': 'clickAddLibrary',
             'click .update-library': 'clickUpdateLibrary',
             'click .remove-library': 'clickRemoveLibrary',
-            'click .reload-page': 'clickReloadPage'
+            'click .reload-page': 'clickReloadPage',
         },
         id: "manage-libs",
         attributes: function() {
             return {
-                'class': "manage-libs settings",
-                'data-side-id': "manage-libs"
+                'class': 'manage-libs settings',
+                'data-side-id': 'manage-libs'
             };
         },
         /**
@@ -29,6 +31,17 @@ var QoobManageLibsView = Backbone.View.extend( // eslint-disable-line no-unused-
             this.controller = options.controller;
             this.storage = options.storage;
         },
+        keyAction: function(evt) {
+            if (evt.keyCode == 13) {
+                this.$(".add-library").click();
+            }
+        },
+        keyupInput: function(evt) {
+            var elem = this.$(evt.currentTarget);
+            if (elem.parents('.container-input-add').hasClass('error') && evt.keyCode != 13) {
+                elem.parents('.container-input-add').removeClass('error');
+            }
+        },
         clickAddLibrary: function(evt) {
             evt.preventDefault();
             var self = this,
@@ -40,9 +53,11 @@ var QoobManageLibsView = Backbone.View.extend( // eslint-disable-line no-unused-
                 this.controller.addLibrary(url, function(error) {
                     self.controller.hideLibraryLoader(elem);
                     if (error) {
-                        console.error(error);
+                        elem.parents('.container-input-add').addClass('error');
                     }
                 });
+            } else {
+                elem.parents('.container-input-add').addClass('error');
             }
         },
         clickUpdateLibrary: function(evt) {
@@ -83,9 +98,10 @@ var QoobManageLibsView = Backbone.View.extend( // eslint-disable-line no-unused-
             var data = {
                 'back': this.storage.__('back', 'Back'),
                 'add_url_library': this.storage.__('add_url_library', 'Add library URL'),
-                'enter_url_library': this.storage.__('enter_url_library', 'Enter URL library'),
+                'enter_url_here': this.storage.__('enter_url_here', 'Enter URL here'),
+                'Add': this.storage.__('Add', 'Add'),
                 'libraries_lng': this.storage.__('libraries', 'Active libraries'),
-                'libraries_attention': this.storage.__('libraries_attention', 'Attention!'), 
+                'libraries_attention': this.storage.__('libraries_attention', 'Attention!'),
                 'libraries_text_part_1': this.storage.__('libraries_after_adding', 'After adding, updating or deleting the libraries don\'t forget to'),
                 'libraries_text_part_2': this.storage.__('libraries_reload', 'reload'),
                 'libraries_text_part_3': this.storage.__('libraries_qoob_page_builder', 'qoob page builder.'),
