cb3c38cf3bbf3313c5ab4421857f9f6d91813ef9
portapro <uvarenkov@gmail.com>
2018-01-10 16:42:56 +0200

    added title to button "delete" and refactoring qoob-settings-view.js

--- skins/simple/js/views/qoob-settings-view.js
+++ skins/simple/js/views/qoob-settings-view.js
@@ -70,12 +70,10 @@ var QoobMenuSettingsView = QoobFieldsView.extend( // eslint-disable-line no-unus
                 _.template(
                     this.storage.getSkinTemplate('menu-settings-preview')
                 )({ 
-                    config: this.config,
                     'back': this.storage.__('back', 'Back'),
                     'moveUp': this.storage.__('moveUp', 'Move'),
                     'moveDown': this.storage.__('moveDown', 'Move'),
-                    'delete': this.storage.__('delete', 'Delete')
-                    
+                    'titleButtonDelete': this.storage.__('delete', 'Delete')
                 })
             ).find('.settings-blocks').html(html);
 
