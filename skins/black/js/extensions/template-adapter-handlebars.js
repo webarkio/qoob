0493b1f08ef55b84999f65dfe8fc562c5044d8cc
portapro <uvarenkov@gmail.com>
2018-02-02 15:56:37 +0200

    removed skin black

--- skins/black/js/extensions/template-adapter-handlebars.js
+++
@@ -1,9 +0,0 @@
-var QoobExtensions = QoobExtensions || {};
-QoobExtensions.templating = QoobExtensions.templating || [];
-
-QoobExtensions.templating['handlebars'] = QoobExtensions.templating['hbs'] = function(template) {
-    var compiledTemplate = Handlebars.compile(template);
-    return function(data) {
-        return compiledTemplate(data);
-    };
-};
--- skins/black/js/extensions/template-adapter-underscore.js
+++
@@ -1,9 +0,0 @@
-var QoobExtensions = QoobExtensions || {};
-QoobExtensions.templating = QoobExtensions.templating || [];
-
-QoobExtensions.templating['underscore'] = function(template) {
-    var compiledTemplate = _.template(template);
-    return function(data) {
-        return compiledTemplate(data);
-    };
-};
