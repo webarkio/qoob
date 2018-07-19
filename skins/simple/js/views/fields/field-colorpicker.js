d17f3f1eba909e564663e7b56b3342bb03162e2e
portapro <uvarenkov@gmail.com>
2018-07-19 12:29:29 +0300

    field-colorpicker.js fixed rgb

--- skins/simple/js/views/fields/field-colorpicker.js
+++ skins/simple/js/views/fields/field-colorpicker.js
@@ -1,92 +1,68 @@
 /*global QoobFieldView*/
 var Fields = Fields || {};
 Fields.colorpicker = QoobFieldView.extend(
-/** @lends Fields.colorpicker.prototype */{
-    className: 'field-color-picker field-group',
-    events: {
-        'click .theme-colors': 'changeColor',
-        'keyup input': 'changeInput'
-    },
-    /**
-     * Change other image
-     * @param {Object} evt
-     */
-    changeColor: function (evt) {
-        var elem = jQuery(evt.currentTarget);
-        // var input = elem.closest('.colorpicker-input');
-        var input = elem.closest('.field-color-picker').find('.colorpicker-input');
-        var color = elem.data('color');
-        
-        input.val(color);
-        
-        input.css('color', this.inputColor(color));
-        input.css({"backgroundColor" : color});
-        
-        this.model.set(input.prop('name'), color);
-    },
-    /**
-     * Transform hex color to rgb color
-     * @param {String} hex - Color that will be converted to rgb
-     */
-    hexToRgb: function (hex) {
-        var bigint = parseInt(hex.replace(/#/, ""), 16);
+    /** @lends Fields.colorpicker.prototype */
+    {
+        className: 'field-color-picker field-group',
+        events: {
+            'click .theme-colors': 'changeListColor',
+            'keyup input': 'changeInput'
+        },
+        checkRgb: function(rgb) {
+            var rxValidRgb = /([R][G][B][A]?[(]\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])(\s*,\s*((0\.[0-9]{1})|(1\.0)|(1)))?[)])/i
+            if (rxValidRgb.test(rgb)) {
+                return true;
+            }
+        },
+        changeInput: function(evt) {
+            var color = jQuery(evt.target).val().trim();
+            this.changeColor(color);
+        },
+        /**
+         * Change other color
+         * @param {Object} evt
+         */
+        changeListColor: function(evt) {
+            var color = jQuery(evt.currentTarget).data('color').trim();
+            this.$el.find('.field-color-picker__input').val(color);
+            this.changeColor(color);
+        },
+        isBlackColor: function (color) {
+            return ['#000', '#000000', 'rgb(0,0,0)'].indexOf(color) !== -1;
+        },
+        changeColor: function(color) {
+            var input = this.$el.find('.field-color-picker__input'),
+                container = this.$el.find('.field-color-picker__container');
 
-        var r = (bigint >> 16) & 255;
-        var g = (bigint >> 8) & 255;
-        var b = bigint & 255;
+            // hex or rgb
+            if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i.test(color) || this.checkRgb(color)) {
+                if (this.isBlackColor(color)) {
+                    input.addClass('field-color-picker__input__light');
+                } else {
+                    input.removeClass('field-color-picker__input__light');
+                }
+                container.css({ "backgroundColor": color });
+                this.model.set(this.name, color);
+            }
+        },
+        /**
+         * Render filed colorpicker
+         * @returns {Object}
+         */
+        render: function() {
+            var htmldata = {
+                "label": this.settings.label,
+                "name": this.settings.name,
+                "value": this.getValue(),
+                "colors": this.settings.colors
+            };
 
-        return "rgb("+ r + "," + g + "," + b + ")";
-    },
-    /**
-     * Color for input text
-     * @param {String} color - Color in hex format that user has entered or selected  
-     * @return {String} Text's color for input element
-     */
-    inputColor: function(color) {
-            var rgbColor = this.hexToRgb(color);
-            
-            var colors = rgbColor.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-            var brightness = 1;
+            this.$el.html(_.template(this.storage.getSkinTemplate('field-colorpicker-preview'))(htmldata));
 
-            var r = colors[1];
-            var g = colors[2];
-            var b = colors[3];
+            if (this.isBlackColor(this.getValue())) {
+                this.$el.find('.field-color-picker__input').addClass('field-color-picker__input__light');
+            }
 
-            var ir = Math.floor((255-r)*brightness);
-            var ig = Math.floor((255-g)*brightness);
-            var ib = Math.floor((255-b)*brightness);
-
-            return 'rgb('+ir+','+ig+','+ib+')';
-    },
-    changeInput: function(evt) {
-        var target = jQuery(evt.target);
-        var color = target.val();
-
-        if( color.length === 7 ) {
-            jQuery(evt.target).css('color', this.inputColor(color));
-            jQuery(evt.target).css({"backgroundColor" : target.val()});
+            return this;
         }
-        
-        this.model.set(target.attr('name'), target.val());
-    },
-    /**
-     * Render filed colorpicker
-     * @returns {Object}
-     */
-    render: function () {
-        if(this.getValue() !== undefined ) {
-           var inputColor = this.inputColor(this.getValue());
-        }
-        var htmldata = {
-            "label" : this.settings.label,
-            "name" : this.settings.name,
-            "value" : this.getValue(),
-            "arr_colors" : jQuery.inArray(this.getValue(), this.settings.colors),
-            "colors" : this.settings.colors,
-            "inputColor": inputColor
-        };
-
-        this.$el.html(_.template(this.storage.getSkinTemplate('field-colorpicker-preview'))(htmldata));
-        return this;
-    }
-});
+    });
